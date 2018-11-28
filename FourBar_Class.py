949841b0a9e73578ff2127012f74215dd54b5bab
TDCrotty124 <45367333+TDCrotty124@users.noreply.github.com>
2018-11-28 10:14:29 -0600

    Add files via upload

---
+++ FourBar_Class.py
@@ -0,0 +1,186 @@
+import numpy as np
+
+from OpenGL.GL import *
+
+from Homeworks.HW10.OpenGL_2D_class import gl2DCircle
+
+
+class style:
+    def __init__(self):
+        self.name = None
+        self.rgb = None
+        self.width = None
+
+# class Link:
+#     def __init__(self):
+#         self.name=None
+#         self.node1name=None
+#         self.node2name=None
+#         self.node1 = None
+#         self.node2 = None
+#         self.length=None
+#         self.angle=None
+
+
+class Fourbar:
+    def __init__(self, ):
+        self.linestyle = []
+        self.connections = []
+        self.payload = []
+        self.positions = None
+        self.boundary = []
+        self.window = [] # an empty list of nodes
+        # self.links = [] # an empty list of links
+        # self.longest = -99999999
+        # self.longestLink = None
+
+
+    # Don't change anything in ReadTrussData
+    def ReadTrussData(self, data):      # reads data from text file
+        #data is an array of strings, read from a Truss data file
+        for line in data:  # loop over all the lines
+            cells = line.strip().split(',')
+            keyword = cells[0].lower()
+
+            if keyword == 'title': self.title = cells[1].replace("'", "")
+            if keyword == 'connections':
+                for con in cells[2:]:
+                    ans = float(con.replace("(", "").replace(")", ""))
+                    self.connections.append(ans)
+
+            if keyword == 'linestyle':
+                this_name = [cells[1].replace("'", "").replace(" ", "")]
+                ncells = len(cells)
+                for cell in cells[2:]:
+                    value = float(cell.replace("(", "").replace(")", ""))
+                    this_name.append(value)
+                self.linestyle.append(this_name)
+
+            if keyword == 'payload':
+                this_load = [cells[1].replace("'", "").replace(" ", "")]
+                ncells = len(cells)
+                for cell in cells[2:]:
+                    value = float(cell.replace("(", "").replace(")", ""))
+                    this_name.append(value)
+                self.linestyle.append(this_name)
+
+
+            if keyword == 'node':
+                thisnode=Node()
+                thisnode.name = cells[1].strip()
+                thisnode.x=float(cells[2].strip())
+                thisnode.y=float(cells[3].strip())
+                self.nodes.append(thisnode)
+
+            if keyword == 'link':
+                thislink = Link()
+                thislink.name = cells[1].strip()
+                thislink.node1name= cells[2].strip()
+                thislink.node2name= cells[3].strip()
+                self.links.append(thislink)
+
+        #end for line
+        self.UpdateLinks()
+
+    # Add 1 more thing which is the viewing size
+    def UpdateLinks(self):
+        #get the node info
+        for link in self.links:
+            for node in  self.nodes:
+                if node.name == link.node1name:
+                    link.node1 = node
+                if node.name == link.node2name:
+                    link.node2 = node
+            #next node
+        #next link
+
+        #get link lengths and angles
+        self.longest = -99999999
+        self.longestLink = None
+        for link in self.links:
+            x1=link.length = link.node1.x
+            y1=link.length = link.node1.y
+            x2=link.length = link.node2.x
+            y2=link.length = link.node2.y
+            link.length = np.sqrt( (x2-x1)**2 + (y2-y1)**2)
+            link.angle = np.arctan2((y2-y1),(x2-x1))
+
+            if link.length > self.longest:
+                self.longest = link.length
+                self.longestLink = link
+
+            # we must create a value to compare to nodes to make sure we are receiving the max and min x&y values
+            xmin = 100000
+            ymin = 100000
+            xmax = -100000
+            ymax = -100000
+
+            for node in self.nodes:    # for loop that sets node.x and node.y to its max and min values to create a certain size for the GL
+                if node.x > xmax:
+                    xmax = node.x
+                if node.x < xmin:
+                    xmin = node.x
+                if node.y > ymax:
+                    ymax = node.y
+                if node.y < ymin:
+                    ymin = node.y
+
+
+
+
+            self.drawingsize = [xmin, xmax, ymin, ymax]
+        #next link
+
+    # # Don't change anything in GenerateReport
+    #   creates the
+    # def GenerateReport(self):
+    #
+    #     rpt ='                      Truss Design Report\n'
+    #     rpt+= '\nTitle: {}\n'.format(self.title)
+    #     rpt+= '\nStatic Factor of Safety: {:6.2f}'.format(self.FStatic)
+    #     rpt+= '\nUltimate Strength: {:9.1f}'.format(self.Sut)
+    #     rpt+= '\nYield Strength: {:9.1f}'.format(self.Sy)
+    #     rpt+= '\nModulus of Elasticity: {:6.1f}'.format(self.E)
+    #     rpt += '\n\n'
+    #
+    #
+    #
+    #     rpt += '------------------- Link Summary -------------------------\n'
+    #     rpt+='\nLink (1)    (2)      Length      Angle\n'
+    #     for link in self.links:
+    #         rpt+='{:6}{:7}{:7}'.format(link.name, link.node1name, link.node2name)
+    #         rpt+='{:7.2f}  '.format(link.length)
+    #         rpt+='{:10.2f}  \n'.format(link.angle)
+    #     rpt += '\n\n'
+    #
+    #     return rpt
+
+
+
+    # Contains all the drawing commands for drawing the link lines
+        # and the nodes as circles
+    def DrawTrussPicture(self):
+
+    # for loop for nodes
+
+    # for loop for links
+
+
+        # this is what actually draws the picture
+        # using data to control what is drawn
+
+
+        glColor3f(0, 0.4, 0.9)  # Changes the color of the circle nodes
+        glLineWidth(1.5)        # controls the width of the line trusses
+        radius = self.longestLink.length / 25       # allows the radius of your circle to format and display correctly as the lengths of the trusses will vary with text files
+        for N in range(len(self.nodes)):    # loops over all nodes and pulls the x and y values from each node
+            gl2DCircle(self.nodes[N].x, self.nodes[N].y, radius, fill=True)     # creates a circle for the node
+
+        # for loop here
+        glColor3f(0, 1, 0.8)    # changes the color of the truss lines
+        for L in range(len(self.links)):        # loop that runs over all of the links
+            glBegin(GL_LINE_STRIP)  # begin drawing connected lines
+            glVertex2f(self.links[L].node1.x, self.links[L].node1.y)    # pulls the location of the node1 and
+            glVertex2f(self.links[L].node2.x, self.links[L].node2.y)    # draws a line from there to node2. These lines create the trusses in the drawing
+            glEnd()
+
---
+++ FourBar_Main.py
@@ -0,0 +1,148 @@
+# standard PyQt5 imports
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
+# standard OpenGL imports
+from OpenGL.GLUT import *
+
+from Homeworks.HW10.OpenGL_2D_class import gl2D
+
+# the ui created by Designer and pyuic
+from FinalProject import Ui_Dialog
+from FourBar_Class import Fourbar
+
+# import the Problem Specific class
+from DroneCatcher import DroneCatcher
+
+
+class main_window(QDialog):
+    def __init__(self):
+        super(main_window, self).__init__()
+        self.ui = Ui_Dialog()
+        # setup the GUI
+        self.ui.setupUi(self)
+
+        # define any data (including object variables) your program might need
+        self.mydrone = DroneCatcher()
+
+        # create and setup the GL window object
+        self.setupGLWindows()
+
+        # and define any Widget callbacks (buttons, etc) or other necessary setup
+        self.assign_widgets()
+
+        # define two data items
+        self.Fourbar = None
+        self.filename = None
+        # self.filename = "D:/Users/Dela/Documents/Dropbox/_Classes/_3403/__F18 - 3403/Homework/Homework 9 - Truss File/Solution and testing/Truss Design Input File 1.txt"
+
+        # show the GUI
+        self.show()
+
+    def assign_widgets(self):  # callbacks for Widgets on your GUI
+        self.ui.pushButton_Exit.clicked.connect(self.ExitApp)
+        self.ui.horizontalSlider_zoom.valueChanged.connect(self.glZoomSlider)
+        self.ui.pushButton_Select.clicked.connect(self.GetFourbar)
+    # Widget callbacks start here
+
+    def glZoomSlider(self):  # I used a slider to control GL zooming
+        zoomval = float((self.ui.horizontalSlider_zoom.value()) / 200 + 0.25)
+        self.glwindow1.glZoom(zoomval)  # set the zoom value
+        self.glwindow1.glUpdate()  # update the GL image
+
+
+    # Setup OpenGL Drawing and Viewing
+    def setupGLWindows(self):  # setup all GL windows
+        # send it the   GL Widget     and the drawing Callback function
+        self.glwindow1 = gl2D(self.ui.openGLWidget, self.DrawingCallback)
+
+        # set the drawing space:    xmin  xmax  ymin   ymax
+        self.glwindow1.setViewSize(-10, 500, -10, 200, allowDistortion=False)
+
+
+    def DrawingCallback(self):
+        # this is what actually draws the picture
+        if self.Fourbar is not None:
+            self.Fourbar.DrawTrussPicture()
+
+    def GetFourbar(self):
+        # get the filename using the OPEN dialog
+        self.filename = QFileDialog.getOpenFileName()[0]
+        if len(self.filename) == 0:
+            no_file()
+            return
+        self.ui.filename.setText(self.filename)
+        app.processEvents()
+
+        # Read the file
+        f1 = open(self.filename, 'r')  # open the file for reading
+        data = f1.readlines()  # read the entire file as a list of strings
+        f1.close()  # close the file  ... very important
+
+        self.Fourbar = Fourbar()  # create a Truss instance (object)
+
+        t = self.Fourbar  # a shorter name for convenience
+
+        t.ReadTrussData(data)
+
+        # rpt = t.GenerateReport()
+
+        # put the report in the large TextBox
+        # self.ui.textEdit_Report.setText(rpt)
+
+        # fill the small text boxes
+        self.ui.lineEdit_a0.setText(t.longestLink.name)
+        self.ui.lineEdit_a.setText(t.longestLink.node1.name)
+        self.ui.lineEdit_StartAngle.setText(t.longestLink.node2.name)
+        self.ui.lineEdit_EndAngle.setText('{:8.2f}'.format(t.longest))
+        self.ui.lineEdit_b0.setText(t.longestLink.node1.name)
+        self.ui.lineEdit_b.setText(t.longestLink.node1.name)
+        self.ui.MouseLocation.setText(t.longestLink.node1.name)
+
+        #draw the picture
+        # this makes sure the window of the GL is slightly bigger to allow the picture of the truss to be visible
+        [xmin,xmax,ymin,ymax] = self.truss.drawingsize
+        dx = xmax - xmin
+        dy = ymax - ymin
+        xmin -= 0.05*dx
+        xmax += 0.05*dx
+        ymin -= 0.05*dy
+        ymax += 0.05*dy
+        self.glwindow1.setViewSize(xmin,xmax,ymin,ymax, allowDistortion=False)
+
+        self.glwindow1.glUpdate()
+
+    def ExitApp(self):
+        app.exit()
+
+# if no file is presented give message that no file was selected
+def no_file():
+    msg = QMessageBox()
+    msg.setText('There was no file selected')
+    msg.setWindowTitle("No File")
+    retval = msg.exec_()
+    return None
+
+# if file is no valid type display message that file is bad
+def bad_file():
+    msg = QMessageBox()
+    msg.setText('Unable to process the selected file')
+    msg.setWindowTitle("Bad File")
+    retval = msg.exec_()
+    return None
+
+
+if __name__ == "__main__":
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
+    app.aboutToQuit.connect(app.deleteLater)
+    main_win = main_window()
+
+    # if a filename exists at the beginning of the program .. open it
+    # ... to save time in the debugging cycle
+    name = main_win.filename
+    if name is not None:
+        main_win.GetFourbar(name=main_win.filename)
+
+    sys.exit(app.exec_())
